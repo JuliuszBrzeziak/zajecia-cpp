3c8e345d23f2a1e94b18867d546093557046f330
JuliuszBrzeziak <juliusz.brzeziak@student.uj.edu.pl>
2019-03-11 17:57:29 +0100

    strinigi cos

---
+++ 1cwiczenia/zad1a.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class myClass
+{
+    public:
+
+
+    string imie, nazwisko, ulica, nr_domu, kod_pocztowy, miejscowosc;
+
+    void getDataa(){
+    cout << "podaj imie";
+    getline(cin, imie);
+
+    cout << "podaj nazwisko";
+    getline(cin, nazwisko);
+
+    cout << "podaj ulica";
+    getline(cin, ulica);
+
+    cout << "podaj nr_domu";
+    getline(cin, nr_domu);
+
+    cout << "podaj kod_pocztowy";
+    getline(cin, kod_pocztowy);
+
+    cout << "podaj miejscowosc";
+    getline(cin, miejscowosc);
+    }
+
+    void printDataa(){
+        cout << "-------------" << endl;
+        cout << imie << " " << nazwisko << endl;
+        cout << ulica << " " << nr_domu << endl;
+        cout << kod_pocztowy << " " << miejscowosc << endl;
+        cout << "-------------" << endl;
+    }
+};
+
+int main(){
+    myClass pas;
+
+    cout << "he" << endl;
+    pas.getDataa();
+    pas.printDataa();
+}
