60ee62036aa3890661efbc3a8d6f63edc4373167
JuliuszBrzeziak <juliusz.brzeziak@student.uj.edu.pl>
2019-06-19 17:41:38 +0200

    umiem rysować bit po bicie

---
+++ projekt/wzorce/mybmp.cpp
@@ -0,0 +1,52 @@
+
+//###############################################################################
+//
+//                   RYSOWANIE BIT PO BICIE
+//
+//##############################################################################
+
+
+
+
+#include <cmath>
+#include <cstdlib>
+#include "bitmap_image.hpp"
+
+struct point_t { double x,y; };
+
+int main()
+{
+    const int canvas_width  = 600;//ustalenie szerokości mapy
+    const int canvas_height = 400;//ustalenie wysokości
+
+    cartesian_canvas canvas(canvas_width,canvas_height); //ustalenie że obraz będzie kartezjański
+
+    //canvas.setup_canvas(255, 150, 50);
+
+    point_t pnt = {200,200};
+    point_t vers ={1,1};
+    for(int i = 0; i<3000;i++){
+        pnt.x -= vers.x;
+        pnt.y -= vers.y;
+
+        canvas.plot_pen_pixel(pnt.x,pnt.y);//rusyje kropkę w danym miejscu
+    }
+
+
+    pnt = {-300,-200}; //ustawiam się w lewym dolnym rogu
+    for (int j = 0; j < 200; j++)
+    {
+        pnt.x += vers.x;
+
+        canvas.plot_pen_pixel(pnt.x, pnt.y);
+    }
+    
+   
+   
+    canvas.image().save_image("mybmp.bmp");//zapisuje
+   
+
+
+
+    return 0;
+}
