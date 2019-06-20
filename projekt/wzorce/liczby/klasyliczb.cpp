6bbcbd3c96b568d990644d837a5c9cce330c46a6
JuliuszBrzeziak <juliusz.brzeziak@student.uj.edu.pl>
2019-06-20 23:50:57 +0200

    działa na wektorach

--- projekt/wzorce/liczby/klasyliczb.cpp
+++ projekt/wzorce/liczby/klasyliczb.cpp
@@ -32,43 +32,79 @@ class Liczba{
     //#############################################################
 
     //bierze tablicę inta i wpisuje go w tabelę tab Liczby
-    //DZIAŁA
-    void Wczytaj_tab(int *tab){
+    //OK
+    void Wczytaj_tab(vector<int> tab){
         this->tab=tab;
     }
 
 
     //bierze inta i wpisuje go w l Liczby
-    //DZIAŁA
+    //OK
     void Wczytaj_l(int a){
         l = a;
     }
 
     //wypisuje i`ty element tablicy
-    //DZIAŁA
+    //OK
     void Wypisz_tabi(int i){
         cout << "element " <<i<<" to "<< tab[i] <<  endl;
     }
     
-    //zwraca wielkość tablicy
-    //NIEDZIAŁA
-    template <typename T> int tab_Size(T tabw){
-        cout << sizeof(tabw)/sizeof(*tabw) << endl;
-        return sizeof(tabw)/sizeof(*tabw);
-    }
-
-    //wypisuje taba liczby
-    //NIEDZIAŁA
-    int tab_Size2(){
-        return sizeof(tab)/sizeof(*tab);
+    //zwraca taba liczby
+    //OK
+    int tab_Size(){
+        return sizeof(tab)/sizeof(tab[0]);
     }
 
     //wypisuje tab
-    //NIEDZIAŁA
+    //OK
     void Wypisz_tab(){
-        for(int k=0;k<sizeof(tab)/sizeof(tab[0]);k++){
-            cout << tab[k] << endl;
+        for(int k=0;k<tab_Size();k++){
+            cout << tab[k] << ",";
         }
+    }
+
+
+    //Funkcja testowa
+    void test(int i,int talbica[],vector<int> vec){
+
+        int tablica2[5]={0,1,2,3,4};
+        int i2=884;
+        vector<int> vec2(5,3);
+
+        //vec2[0]=88;
+        //vec2[1]=881;
+        vec2[2]=882;
+        //vec2[3]=883;
+        
+
+
+        //sprawdza działanie wczytaj_tab wewnętrznego wektora
+        Wczytaj_tab(vec2);
+        cout <<"wczytaj_tab wewnątrz:" <<tab[2] << "  jeżeli 882 to dobrze"<< endl;
+
+        //sprawdza wczytaj_tab wektorów zewnętrznych
+        Wczytaj_tab(vec);
+        cout <<"wczytaj_tab zewnątrz:" <<tab[2] << "  jeżeli 883 to dobrze"<< endl;
+
+        //sprawdza działanie wewnętrznego wczytaj_l
+        Wczytaj_l(i2);
+        cout <<"wczytaj_l wewnątrz:" <<i2 << "  jeżeli 884 to dobrze"<< endl;
+
+        //sprawdza działanie zewnętrznego wczytaj_l
+        Wczytaj_l(i);
+        cout <<"wczytaj_tab wewnątrz:" <<l << "  jeżeli 885 to dobrze"<< endl;
+
+        //sprawdza wypisz_tabi
+        Wypisz_tabi(2);
+
+        //sprawdza tab_size
+        cout << "tab_size " << tab_Size() << " powinno być 3" << endl;
+
+        //sprawdza wypisz_tab
+        cout << "wypisz tab";
+        Wypisz_tab();
+        cout << "powinno być \"883,883,883,\" "<<endl;
     }
 
 };
@@ -76,17 +112,16 @@ class Liczba{
 int main(){
 
     Liczba licz;
-    int arr[3]={0,1,2};
+    int arr[3]={0,1,882};
 
     int *ar;
     ar = arr;
-    vector <int> arv(3,2);
+    vector <int> arv(3,883);
 
-    licz.Wczytaj_tab(arr);
+    licz.test(885,arr,arv);
 
-    
+    //licz.Wczytaj_tab(arv);
 
-    cout << sizeof(arv)/sizeof(arv[1]) <<endl;
 
 
 
