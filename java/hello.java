53c016e6f5ba48b91320f36797408fa7f8a511c1
Juliusz Brzeziak <juliusz.brzeziak@student.uj.edu.pl>
2019-10-08 20:36:30 +0200

    qwert

--- java/hello.java
+++ java/hello.java
@@ -1,15 +1,13 @@
 class hello {
     public static void main(String[] args) {
-        
-    	
-            byte range;
-            
-            range = 24;
-            //range = 200;
-            System.out.println(range);
-            
-            // Error code below. Why?
-             
-         
+
+        byte range;
+
+        range = 24+;
+        // range = 200;
+        System.out.println(range);
+
+        // Error code below. Why?
+
     }
 }
