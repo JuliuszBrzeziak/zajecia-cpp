55af25e063a9ba515e8ce9302d2bf87d5b656d9a
Juliusz <juliusz.brzeziak@student.uj.edu.pl>
2019-12-17 03:10:27 +0100

    klasa multi ma DZIAŁAJĄCY interfejs Test(na razie nie generyczny)

--- java/zad2/machines/Abc.java
+++ java/zad2/machines/Abc.java
@@ -1,24 +1,7 @@
 public class Abc {
+    
     public static void main(String[] args) {
- 
-        Sum<Integer,Integer,Integer> addTwoIntegers  = new Sum<Integer, Integer, Integer>() {
-            @Override
-            public Integer sum(Integer number1, Integer number2) {
-                return number1 + number2;
-            }
-        };
- 
-        Sum<String,String,String> addTwoStrings  = new Sum<String, String, String>() {
-            @Override
-            public String sum(String string1, String string2) {
-                return string1.concat(string2);
-            }
-        };
 
-        //Computation<Double> cmp = new Computation<Double>(){};
- 
-        System.out.println(addTwoIntegers.sum(10,30));
-        System.out.println(addTwoStrings.sum("Hello!","World"));
         
  
     /*
@@ -30,26 +13,27 @@ public class Abc {
         };
         System.out.println(cmp.compute(3.0));*/
 
-        Multi m = new Multi(3);
-        Computation<Integer> cmp = new Multi(3);
-    }
-};
+        
+       // Computation<Integer> cmp = new Multi(3);
+       Test testa = new Multi(3);
 
-class Multi {
+       System.out.println(testa.test());
+    
+}}
+
+class Multi implements Test {
     private int a;
     public Multi(int a){
         this.a = a;
     }
 
-    
+
+        @Override
+        public int test(){
+            return 2;
+            };
+
 
 }
  
-interface Sum<T,U,V>{
-      V sum(T number1, U number2);
-}
 
- 
-interface Computation<T>{
-      T compute(T number1);
-}
