cfa24d56a077f85b31408ac2c9eee222af1490e2
Juliusz <juliusz.brzeziak@student.uj.edu.pl>
2019-12-17 03:45:06 +0100

    interfejs generyczny

--- java/zad2/machines/Abc.java
+++ java/zad2/machines/Abc.java
@@ -2,35 +2,41 @@ public class Abc {
     
     public static void main(String[] args) {
 
-        
- 
-    /*
-        Computation<Integer> cmp = new Multi(3){
-            @Override
-            public Integer compute(Integer b){
-                return this.a * b;
-            }
-        };
-        System.out.println(cmp.compute(3.0));*/
 
-        
-       // Computation<Integer> cmp = new Multi(3);
-       Test testa = new Multi(3);
+       Test<Double> testa = new Multi(3.0);
 
-       System.out.println(testa.test());
-    
-}}
+       System.out.println(testa.test(4.0));
 
-class Multi implements Test {
-    private int a;
-    public Multi(int a){
+       plot(0,4,testa);
+    }
+
+
+    public static void plot(double xmin, double xmax, Test<Double> f) {
+        int n = 20;
+        double x = xmin;
+        double dx = (xmax - xmin) / n;
+        System.out.println(dx);
+        System.out.println("x0" + x);
+        for(int i = 0; i < 20; i++){
+            System.out.println("f" + f.test(x));
+            System.out.println("x" + x);
+            x += dx;
+        }
+     }
+
+
+}
+
+class Multi implements Test<Double> {
+    private Double a;
+    public Multi(Double a){
         this.a = a;
     }
 
 
         @Override
-        public int test(){
-            return 2;
+        public Double test(Double b){
+            return b * a;
             };
 
 
