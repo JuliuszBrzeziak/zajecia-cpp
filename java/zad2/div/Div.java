97bbecb3bfbdf4107fa3c939138d1acd90f7afd0
Juliusz <juliusz.brzeziak@student.uj.edu.pl>
2019-12-18 06:47:52 +0100

    cos co dziala

--- java/zad2/div/Div.java
+++ java/zad2/div/Div.java
@@ -9,12 +9,7 @@ public class Div {
 
         
 
-                try {
             d.dig(1,0);
-        }
-        catch (IOException e) {
-            System.out.println("aaaa");
-            e.printStackTrace();
         }
 
     }
