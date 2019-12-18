6992184c26438eb0e4fe92673cb1f8d362ba27fa
Juliusz <juliusz.brzeziak@student.uj.edu.pl>
2019-12-18 06:47:36 +0100

    cos co dziala

---
+++ java/zad2/div/Div.java
@@ -0,0 +1,47 @@
+//package pl.samouczekprogramisty.kursjava.exception;
+import java.io.*;
+
+public class Div {
+    public static void main(String[] args) {
+        
+        Div d = new Div();
+
+
+        
+
+                try {
+            d.dig(1,0);
+        }
+        catch (IOException e) {
+            System.out.println("aaaa");
+            e.printStackTrace();
+        }
+
+    }
+
+    public static double dig(double a, double b) throws IOException {
+        try {
+            Div0 instance = new Div0(1.0,b);
+        }
+        catch (IOException e) {
+                        
+System.out.println("asssaaa");
+            e.printStackTrace();
+        }
+        return a/b;
+    }
+
+    private void methodWithCheckedException() throws IOException {
+        throw new IOException();
+    }
+}
+class Div0 extends java.lang.Exception {
+
+    String com;
+    public  Div0(double a, double b) throws IOException {
+        if(b==0.0){
+            System.out.println("asssaaa");
+            throw new IOException();
+        };
+    }
+}
