d1133b271f03c83437d0f9b98feb670effb4c995
Juliusz <juliusz.brzeziak@student.uj.edu.pl>
2019-12-18 07:16:13 +0100

    szkielet w div2

--- java/zad2/div/Div2.java
+++ java/zad2/div/Div2.java
@@ -1,27 +1,27 @@
 class MyException extends Exception {
-   int id;
+   String com;
 
-   public MyException(int x) {
-      id = x;
+   public MyException(String a, String b) {
+      com = x;
    }
 
    public String toString() {
-      return "CustomException[" + id + "]";
+      return "CustomException[" + com + "]";
    }
 }
 
 public class Div2 {
-   static void compute(int a) throws MyException {
-      if (a > 10)
-         throw new MyException(a);
+   static void compute(double a,double b) throws MyException {
+      if (b == 0)
+         throw new MyException("a","0");
          System.out.println("No error in prog. no exception caught");
    }
 
    public static void main(String args[]) {
    
       try {
-         compute(5);
-         compute(12);
+         compute(5,1);
+         compute(12,0);
       } catch(MyException ex1) {
          System.out.println(ex1);
       }
