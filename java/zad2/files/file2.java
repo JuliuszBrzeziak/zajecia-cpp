02831ab578349df84c7adba9b370b84054059c0b
Juliusz <juliusz.brzeziak@student.uj.edu.pl>
2019-12-18 04:04:39 +0100

    file2 to działająca pierwsza część zadania

--- java/zad2/files/file2.java
+++ java/zad2/files/file2.java
@@ -4,9 +4,10 @@ import java.io.*;
 public class file2 {
 
     public static void main(String[] args) {
-        File file = new File("test.txt");
-        String filePath = "test2.txt";
+        File file = new File(args[0]);
+        String filePath = args[1];
             int number = 1234567;
+            int i = 0;
             //FileWriter fileWriter = null;
 
         try (var br = new BufferedReader(new FileReader(file));
@@ -16,7 +17,9 @@ public class file2 {
 
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
-                fileWriter.write(Integer.toString(number));
+                fileWriter.write(Integer.toString(i ));
+                fileWriter.write(line+"\n");
+                i++;
             }
         } catch (FileNotFoundException e) {
             System.out.println("Can't find file " + file.toString());
