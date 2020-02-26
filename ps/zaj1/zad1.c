93b5fecf0e5ca7c5ce3ec5f967242af6cfa0aa9d
Juliusz <juliusz.brzeziak@student.uj.edu.pl>
2020-02-26 14:44:27 +0100

    poprawka, teraz zad1

--- ps/zaj1/zad1.c
+++ ps/zaj1/zad1.c
@@ -1,6 +1,7 @@
 /* Napisz (albo odszukaj w archiwum zadań domowych z pierwszego roku studiów) program 
 w C deklarujący tablicę int liczby[50] i wczytujący do niej z klawiatury kolejne liczby.
- Wczytywanie należy przerwać gdy użytkownik wpisze zero lub po wczytaniu 50 liczb.*/
+ Wczytywanie należy przerwać gdy użytkownik wpisze zero lub po wczytaniu 50 liczb.
+ Dodaj do powyższego programu pętlę for idącą przez wszystkie wczytane liczby i drukującą te z nich, które są większe od 10 i mniejsze od 100.*/
 
  #include <stdio.h>
 
@@ -13,6 +14,12 @@ w C deklarujący tablicę int liczby[50] i wczytujący do niej z klawiatury kole
         scanf("%d",&liczby[i]);
         if(liczby[i]==0){
             break;
+        }
+    }
+
+    for(int j = 0; j <50;j++ ){
+        if(liczby[j]>10 && liczby[j]<100){
+            printf("%d",liczby[j]);
         }
     }
 
