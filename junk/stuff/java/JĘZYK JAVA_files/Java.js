607a7782613f62e570467cdc17b29a6d66be501b
juliusz <juliusz.brzeziak@student.uj.edu.pl>
2020-03-03 16:40:21 +0100

    usun

--- junk/stuff/java/JĘZYK JAVA_files/Java.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20180911
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Java_files/JavaMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();fixupAllIEPNGBGs();Widget.onload();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
